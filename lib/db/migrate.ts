2dd5b2779614c11c6473f50253f9a80eb09578cb
Kavinraj <kavinask007@gmail.com>
2024-12-26 12:35:04 +0530

    added debug log to check

--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -11,7 +11,7 @@ const runMigrate = async () => {
   if (!process.env.POSTGRES_URL) {
     throw new Error('POSTGRES_URL is not defined');
   }
-
+  console.log(process.env.POSTGRES_URL)
   const connection = postgres(process.env.POSTGRES_URL, { max: 1 });
   const db = drizzle(connection);
 
